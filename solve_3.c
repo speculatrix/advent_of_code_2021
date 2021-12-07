f30d9664059c1a576ce4dd88634428c465299adb
Paul Mansfield <pmansfield@illumina.com>
2021-12-07 14:15:14 +0000

    added file read & line read

--- solve_3.c
+++ solve_3.c
@@ -1,12 +1,29 @@
 /* solve_3.c */
 
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[])
-{
+#define INPUT_3 "3_input.txt"
+
+int main(int argc, char *argv[]) {
 	printf("hello world\n");
 
+	FILE	*fp_in;
+	char	*input_line = NULL;
+	size_t	input_len = 0;
 
+	if (fp_in = fopen(INPUT_3, "r")) {
+		while (getline(&input_line, &input_len, fp_in) != -1) {
+
+			printf("input_line %s\n", input_line);
+		}
+
+		// split input line
+	}
+	else {
+		printf("Error, failed to open file %s for reading", INPUT_3);
+		exit(1);
+	}
 
 	return(0);
 }
